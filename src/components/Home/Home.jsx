efcb676f4bfa1d8340e8979bb31eefdfd7b2e659
Tareq1131 <tareqislam076@gmail.com>
2023-10-23 00:57:16 +0600

    add remove from cart and not allow duplicate

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,51 @@
 //import React from 'react'
 
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from "react-router-dom";
+import TShirt from "../Tshirt/TShirt";
+import Cart from "../Cart/Cart";
+import "./Home.css";
+import { useState } from "react";
+import toast from 'react-hot-toast';
 
 const Home = () => {
-    const tshirts = useLoaderData();
-  return (
-    <div>
-      <h2>This is home:{tshirts.length}</h2>
-    </div>
-  )
-}
+  const tshirts = useLoaderData();
+  const [cart, setCart] = useState([]);
 
-export default Home
+  const handleAddToCart = tshirt => {
+    //console.log(tshirt)
+    const exists = cart.find(ts => ts._id === tshirt._id);
+    if(exists){
+        toast('You have already added this t-shirt');
+    }
+    else{
+        const newCart = [...cart, tshirt];
+        setCart(newCart);
+    }
+    
+  };
+
+  const handleRemoveFromCart = id => {
+   // console.log(id);
+    const remaining =cart.filter(ts=>ts._id!== id);
+    setCart(remaining);
+  };
+
+  return (
+    <div className="home-container">
+      <div className="t-shirts-container">
+        {tshirts.map((tshirt) => (
+          <TShirt
+            key={tshirt._id}
+            tshirt={tshirt}
+            handleAddToCart={handleAddToCart}
+          ></TShirt>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
